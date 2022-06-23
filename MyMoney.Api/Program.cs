dfaa9dbc11f458b61f9504a27dc8dcc990da3346
Kinh Nguyen Van <KinhNV@batdongsan.com.vn>
2022-06-23 17:44:56 +0700

    update

--- MyMoney.Api/Program.cs
+++ MyMoney.Api/Program.cs
@@ -7,15 +7,15 @@ var builder = WebApplication.CreateBuilder(args);
 
 ConfigurationManager configuration = builder.Configuration;
 
-builder.Services.AddDbContext<ApplicationDbContext>(optionsBuilder =>
-{
-    optionsBuilder.UseSqlServer(configuration["Mssql:ConnectionString"]);
-});
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<ApplicationDbContext>(optionsBuilder =>
+{
+    optionsBuilder.UseSqlServer(configuration["Mssql:ConnectionString"]);
+});
 
 var app = builder.Build();
 
